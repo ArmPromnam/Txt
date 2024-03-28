00d8ee63e1431f2092b212f4023f185982d7f8eb
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 11:32:27 +0700

    .

--- MyApp.py
+++ MyApp.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandast as pd
 
 st.title("website Devalope using Python")
 st.header("website Devalope using Python")
 st.subheader("ArmPromnam")
-st.image("11.jpg")
+st.image("12.jpg")
+
+dt=pd.read_csv('./data/iris.csv')
+dt.head(10)
