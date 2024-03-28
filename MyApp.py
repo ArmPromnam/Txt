251c135057154fed45401f09651214137e11a545
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 12:09:25 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -21,8 +21,8 @@ cl4.write(dt['sepal.width'].sum())
 
 dx=pd.DataFrame(
     {
-        "col1":dt['sepat.length'].sum()
-        "col2":dt['sepat.width'].sum()
+        "col1":dt['sepat.length'].sum(),
+        "col2":dt['sepat.width'].sum(),
     }
 )
 st.bar_chart(dx,x='col1',y='col2',color='col3')
