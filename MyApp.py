47d54954891a4e90286a3a9ba5483f4613e8e73a
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 12:07:27 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -18,9 +18,15 @@ cl1.write(dt['sepal.length'].sum())
 cl2.write(dt['sepal.width'].sum())
 cl3.write(dt['sepal.length'].sum())
 cl4.write(dt['sepal.width'].sum())
-cols=['sepat.length','sepal.width','sepat.length']
-dx=dt[cols]
-st.bar_chart(dx,x='sepat.length',y='sepal.width',color='sepat.length')
+
+dx=pd.DataFrame(
+    {
+        "col1:dt['sepat.length'].sum()
+        "col2:dt['sepat.width'].sum()
+        "col3:dt['sepat.length'].sum() 
+    }
+)
+st.bar_chart(dx,x='col1',y='col2',color='col3')
 
 st.write('ค่าเฉลี่ย')
 cl1,cl2,cl3,cl4=st.columns(4)
