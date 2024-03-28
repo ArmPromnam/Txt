a3e2f06cd5c4ac7db27b8d2e25a86049596aef93
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 11:34:13 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -4,7 +4,7 @@ import pandast as pd
 st.title("website Devalope using Python")
 st.header("website Devalope using Python")
 st.subheader("ArmPromnam")
-st.image("12.jpg")
+st.image("12.gif")
 
 dt=pd.read_csv('./data/iris.csv')
 st.write(dt.head(10))
