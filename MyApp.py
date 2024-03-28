286e31528441e87099a5238d5349ea26ede38e65
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 11:45:21 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -1,24 +1,26 @@
 import streamlit as st
 import pandas as pd
 
-st.title("website Devalope using Python")
-st.header("website Devalope using Python")
+st.title("เว็บไซต์พัฒนาด้วย Python")
+st.header("เว็บไซต์พัฒนาด้วย Python")
 st.subheader("ArmPromnam")
 st.image("12.gif")
 
-dt=pd.read_csv('./data/iris.csv')
+dt = pd.read_csv("./data/iris.csv")
 
 st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
 
-st.subheader("สถิติข้อฒุลดอกไม้ Iris")
-st.write('ผลรวม')
-cl1,cl2,cl3,cl4=st.colums(4)
-cl1.wrte(dt['sepal.length'].sum))
-cl2.wrte(dt['sepal.width'].sum))
-cl3.wrte(dt['sepal.length'].sum))
-cl4.wrte(dt['sepal.width'].sum))
-st.write('ค่าเฉลี่ย')
-st.write('ผลรวม')
-st.write('ผลรวม')
+st.subheader("สถิติข้อมูลดอกไม้ Iris")
+
+cl1, cl2, cl3, cl4 = st.columns(4)
+
+cl1.write("ผลรวม")
+cl2.write("ค่าเฉลี่ย")
+cl3.write("ผลรวม")
+cl4.write("ผลรวม")
+
+st.write(dt.sum(axis=0))
+st.write(dt.mean(axis=0))
+st.write(dt.sum(axis=0))
 
