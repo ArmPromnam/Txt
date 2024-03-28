e0ac569d794a8635b60b9d5a1f10a5898075e0e1
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 11:53:47 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -6,7 +6,7 @@ st.header("website Devalope using Python")
 st.subheader("ArmPromnam")
 st.image("12.gif")
 
-dt=pd.read_csv('./data/iris.csv')
+dt=pd.read_csv('data/iris.csv')
 
 st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
