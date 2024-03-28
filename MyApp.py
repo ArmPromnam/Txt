723da6ab5e73c0f3cfcac185470c44177f6d244a
ArmPromnam <armpromnam67@gmail.com>
2024-03-28 11:50:57 +0700

    Update MyApp.py

--- MyApp.py
+++ MyApp.py
@@ -11,11 +11,4 @@ dt=pd.read_csv('./data/iris.csv')
 st.subheader("ข้อมูลดอกไม้ Iris")
 st.write(dt.head(10))
 
-st.subheader("สถิติข้อฒุลดอกไม้ Iris")
-st.write('ผลรวม')
-cl1,cl2,cl3,cl4=st.colums(4)
-cl1.write(dt['sepal.length'].sum())
-cl2.write(dt['sepal.width'].sum())
-cl3.write(dt['sepal.length'].sum())
-cl4.write(dt['sepal.width'].sum())
 
